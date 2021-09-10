fe3d98affc177eda16c980fe952ffb52d22c1fac
Paulo Rafael Ramalho <prafael.ramalho@gmail.com>
2021-09-09 21:28:38 -0300

    Update ft_itoa.c

--- part2/ft_itoa.c
+++ part2/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 15:19:26 by prafael-          #+#    #+#             */
-/*   Updated: 2021/09/09 17:10:45 by prafael-         ###   ########.fr       */
+/*   Updated: 2021/09/09 19:03:36 by prafael-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static size_t	ft_count_size(long int n)
 		return (1 + ft_count_size(n / 10));
 }
 
-static void	ft_populete_loop(char *new, int n, int size)
+static void	ft_populate_loop(char *new, int n, int size)
 {
 	if (n < 0)
 	{
@@ -30,7 +30,7 @@ static void	ft_populete_loop(char *new, int n, int size)
 		n *= -1;
 	}
 	if (n > 9)
-		ft_populete_loop(new, n / 10, size - 1);
+		ft_populate_loop(new, n / 10, size - 1);
 	new[size] = (n % 10) + 48;
 }
 
@@ -51,7 +51,7 @@ char	*ft_itoa(int n)
 	if (!int_str)
 		return (NULL);
 	int_str[size--] = 0;
-	ft_populete_loop(int_str, n, size);
+	ft_populate_loop(int_str, n, size);
 	if (flag == 1)
 		int_str[size] += flag;
 	return (int_str);
