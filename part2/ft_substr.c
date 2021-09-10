bcafa4b1ac9c3a1e2020533e7edb1d1e2a78b296
Paulo Rafael Ramalho <prafael.ramalho@gmail.com>
2021-09-09 21:28:49 -0300

    libft 42 prafael-

--- part2/ft_substr.c
+++ part2/ft_substr.c
@@ -6,13 +6,11 @@
 /*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 15:19:50 by prafael-          #+#    #+#             */
-/*   Updated: 2021/09/03 13:21:04 by prafael-         ###   ########.fr       */
+/*   Updated: 2021/09/09 21:18:26 by prafael-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-//a vida eh loka
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -27,10 +25,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s_len < start + len)
 	{
 		res = ft_calloc(s_len - start + 1, sizeof(char));
+		if (!res)
+			return (NULL);
 		ft_strlcpy(res, s + start, s_len - start + 1);
 		return (res);
 	}
 	res = ft_calloc(len + 1, sizeof(char));
+	if (!res)
+		return (NULL);
 	ft_strlcpy(res, s + start, len + 1);
 	return (res);
 }
