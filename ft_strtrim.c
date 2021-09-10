fd30f11be373dc75fe0910c1ba5a81dbadc9f3ff
Paulo Rafael Ramalho <prafael.ramalho@gmail.com>
2021-09-10 14:22:13 -0300

    libft 42 prafael-

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,69 +6,22 @@
 /*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 15:19:48 by prafael-          #+#    #+#             */
-/*   Updated: 2021/09/09 21:11:55 by prafael-         ###   ########.fr       */
+/*   Updated: 2021/09/10 11:00:03 by prafael-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_start(char const *s1, char const *set, size_t start_find);
-static size_t	get_end(char const *s1, char const *set, size_t start_find);
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	str_len;
-	size_t	start;
-	size_t	end;
-	char	*res;
+	size_t	i;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	str_len = ft_strlen(s1);
-	start = get_start(s1, set, 0);
-	end = get_end(s1, set, str_len);
-	res = ft_substr(s1, start, str_len - end - start);
-	return (res);
-}
-
-static size_t	get_start(char const *s1, char const *set, size_t start_find)
-{
-	size_t	count;
-	size_t	i;
-
-	count = 0;
-	while (start_find == count)
-	{
-		i = 0;
-		while (set[i] != 0)
-		{
-			if (s1[start_find] == set[i])
-				count++;
-			i++;
-		}
-		start_find++;
-	}
-	return (count);
-}
-
-static size_t	get_end(char const *s1, char const *set, size_t start_find)
-{
-	size_t	end;
-	size_t	count;
-	size_t	i;
-
-	count = 0;
-	end = ft_strlen(s1);
-	while (end - start_find == count)
-	{
-		i = 0;
-		while (set[i] != 0)
-		{
-			if (s1[start_find - 1] == set[i])
-				count++;
-			i++;
-		}
-		start_find--;
-	}
-	return (count);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
